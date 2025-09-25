aaf11307b7edffe1d4475037149e209c1e53d6b9
JosephMayfield <113706426+JosephMayfield@users.noreply.github.com>
2025-09-25 09:34:06 -0400

    Add files via upload

---
+++ mgmt4.ps1
@@ -0,0 +1,15 @@
+﻿# Define process and URL
+$processName = "chrome"
+$url = "https://www.champlain.edu"
+
+# Check if Chrome is running
+if (-not (Get-Process -Name $processName -ErrorAction SilentlyContinue)) {
+    # Chrome not running, so start it and go to Champlain.edu
+    Start-Process $processName $url
+    Write-Host "Chrome started and directed to Champlain.edu"
+}
+else {
+    # Chrome running, so stop it
+    Get-Process -Name $processName | Stop-Process -Force
+    Write-Host "Chrome was running and has been stopped"
+}
