37c3d5883e0268cd4bbc7c44f902a4d2382ffb09
JosephMayfield <113706426+JosephMayfield@users.noreply.github.com>
2025-10-09 15:24:47 -0400

    Add files via upload

---
+++ Week 4/Scrap1.ps1
@@ -0,0 +1,20 @@
+﻿$scraped_page = Invoke-WebRequest -TimeoutSec 10 http://10.0.17.38/ToBeScraped.html
+
+# Get a count of the links in the page
+$scraped_page.count
+
+#Display links as HTML Element
+$scraped_page.Links  
+
+#Display only URL and its text
+$scraped_page.Links | Select-Object outerText,href
+
+$h2s=$scraped_page.ParsedHtml.body.getElementsByTagName("h2") | Select-Object outerText
+
+$h2s
+
+#print innerText of every div element that has the classs as "div-1"
+$divs1=$scraped_page.ParsedHtml.body.getElementsByTagName("div") | Where-Object {
+$_.getAttributeNode("class").Value -ilike "*div-1"} | select innerText
+
+$divs1
